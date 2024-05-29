f3b3b70bff6a77e6c6cc9af0df95b05ec3b7852e
oyatillo0509 <tilloxon0509@gmail.com>
2024-05-29 12:48:14 +0500

    finshed

---
+++ js/index.js
@@ -0,0 +1,89 @@
+// 1-masala
+
+// let a=+prompt("sm larni kiriting");
+// a=parseInt(a/100);
+// console.log(a);
+
+// 2-masala
+
+// let m=+prompt("Og'irlikni kg larda kiriting", 15);
+// m=parseInt(m/1000);
+// console.log(m);
+
+// 3-masala
+
+// let b=+prompt("hajmini krit?")
+// let a=parseInt(b/1024)
+// console.log(a);
+
+// 4-masala
+
+// let a=+prompt("1-sonni kiriting");
+// let b = +prompt("2-sonni kiriting");
+// let c=parseInt(a/b)
+// console.log(c);
+
+// 5-masala
+
+// let a = +prompt("1-sonni kiriting");
+// let b = +prompt("2-sonni kiriting");
+// let c = parseInt(a / b);
+// let qol = a % b;
+// console.log(c);
+// console.log(qol);
+
+// 6-masala
+
+// let a=+prompt("son kiriting");
+// let on=parseInt(a/10);
+// let bir=a%10;
+// console.log(on);
+// console.log(bir);
+
+// 7-masala
+
+// let b=+prompt("son kiriting");
+// let on=parseInt(b/10);
+// let bir=b%10;
+// let y=on + bir
+// console.log(y);
+
+// 8-masala
+
+// let c = +prompt("son kiriting");
+// let on = Math.floor(c / 10);
+// let bir = c % 10;
+// let num = `${bir}${on}`;
+// console.log(num);
+
+// 9-masala
+
+// let d = +prompt("son kiriting");
+// let yuz = Math.floor(d / 100);
+// console.log(yuz);
+
+// 10-masala
+
+// let e = +prompt("3 xonali son");
+// let bir = e % 10;
+// let on = Math.floor((e % 100) / 10);
+// console.log("onlik: " + on);
+// console.log("birlik: " + bir);
+
+// 11-masala
+
+// let a = +prompt("3 xonali son kirit");
+// let bir = a % 10;
+// let on = Math.floor((a % 100) / 10);
+// let yuz = Math.floor(a / 100);
+// let y = bir + on + yuz;
+// console.log(y);
+
+// 12-masala
+
+// let a = +prompt("3 xonali son");
+// let on = Math.floor((a % 100) / 10);
+// let bir = c % 10;
+// let yuz = Math.floor(a / 100);
+// let num = `${bir}${on}${yuz}`;
+// console.log(num);
